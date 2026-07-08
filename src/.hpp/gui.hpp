901e03a40cb6d1bdcf1664ae715d8190b3d83db1
Commonwealthrocks <204951789+Commonwealthrocks@users.noreply.github.com>
2026-07-08 19:33:42 +0200

    gui.hpp; connect IPC server to archive creation logic

--- src/.hpp/gui.hpp
+++ src/.hpp/gui.hpp
@@ -1,18 +1,25 @@
 // gui.hpp
-// last updated: 28/06/2026
+// last updated: 08/07/2026
 #pragma once
 #include <QMainWindow>
 #include <QPushButton>
+namespace pk::ipc
+{
+    class ipc_server;
+}
+namespace pk::ui::outs
+{
+    class cd_mk_archive;
+}
 namespace pk::ui
 {
     class gui : public QMainWindow
     {
         Q_OBJECT
     public:
-        explicit gui(QWidget *parent = nullptr);
+        explicit gui(pk::ipc::ipc_server *ipc, QWidget *parent = nullptr);
         ~gui() override = default;
         void handle_args(const QString &mode, const QString &path);
-
     private slots:
         void on_create_archive_clicked();
         void on_decrypt_archive_clicked();
@@ -25,6 +32,7 @@ namespace pk::ui
         void dark_theme();
         QPushButton *btn_mk;
         QPushButton *btn_decrypt;
+        pk::ui::outs::cd_mk_archive *m_mk_archive_dialog = nullptr;
     };
 }
 
